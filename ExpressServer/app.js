b6510c859e59aeff489581b1c32b62dc67d6c733
antolube20 <antolube20@gmail.com>
2021-07-26 20:02:26 -0300

    Se pasa  rama de prueba , no se conecta a base de datos

--- ExpressServer/app.js
+++ ExpressServer/app.js
@@ -8,6 +8,7 @@ const passport = require('passport');
 const passportConfig = require("./config/server/passportConfig")
 const config = require('config');
 const apiPrefix = config.get('apiPrefix');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false}))
 
@@ -24,6 +25,17 @@ app.use("/",router)
 // pass passport for configuration
 passport.use(passportConfig.createStrategy())
 app.use(passport.initialize())
+
+// Configurar cabeceras y cors
+app.use(cors())
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
+
 
 app.listen(port,()=>{
 console.log(`Server listen at http:localhost:${port}`);
--- ExpressServer/routes/usersRoutes.js
+++ ExpressServer/routes/usersRoutes.js
@@ -6,20 +6,21 @@ const userController = require('../controller/userController');
 const {required} = require('../middlewares/auth');
 //las rutas del usuario
 
+//acceso protejido con middlewares
 router.get('/',[required], (req, res) =>{
     routeController.handleRequest(req, res,userController.getAll)
 })
-router.get('/:id', (req, res) =>{
+router.get('/:id',[required], (req, res) =>{
     routeController.handleRequest(req, res,userController.getById)
 })
 // router.post('/', (req, res) =>{
 //     routeController.handleRequest(req, res,userController.create)
 // })
-router.patch('/:id', (req, res) =>{
+router.patch('/:id',[required], (req, res) =>{
     routeController.handleRequest(req, res,userController.updated)
 })
 
-router.delete('/:id', (req, res) =>{
+router.delete('/:id',[required], (req, res) =>{
     routeController.handleRequest(req, res,userController.deleted)
 })
 
