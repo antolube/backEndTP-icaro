da2685af21b8f86f8a0aa50d94ab79d55d0efea4
antolube20 <antolube20@gmail.com>
2021-08-11 16:57:16 -0300

    adding recipient_name to msgSent, OK

--- ExpressServer/services/messageServices.js
+++ ExpressServer/services/messageServices.js
@@ -5,7 +5,7 @@ const userModel = require('../models/userModel');
 
 const getAllService = async({user_name,id_user})=>{
 
-    console.log("get All User Diary - condition : "+user_name +"--"+id_user);
+    // console.log("get All User Diary - condition : "+user_name +"--"+id_user);
     const where = {}
     if (user_name){
         where. user_name = user_name;
@@ -17,7 +17,7 @@ const getAllService = async({user_name,id_user})=>{
     const usuarios = await userModel.findAll({attributes:['user_name','id_user','email'],
     where:where});
     // const usuarios = await userModel.findAll({condition,email})
-    console.log(" User Diary return :" +JSON.stringify(usuarios));
+    // console.log(" User Diary return :" +JSON.stringify(usuarios));
     return usuarios;
 }
 
@@ -31,24 +31,31 @@ const createMessage = async ({message,sender_id,sender_name,recipient_id,recipie
     if (recipient_id){
         where. id_user =  recipient_id;
     }
-    
+    //  const recipient_name ={
+    // }
+    const user_name = JSON.parse(JSON.stringify(await userModel.findOne({attributes:['user_name'],where:where})));
+
+
+    console.log("estoy recieciendo este user_name" + JSON.stringify(user_name));
+
     const data = {
         message: message,
         sender_id: sender_id,
         sender_name: sender_name,
         recipient_id:recipient_id,
-        recipient_name: JSON.stringify(await userModel.findOne({attributes:['user_name'],where:where})),
+        recipient_name: user_name.user_name,
         created_at: Date(),
     }
 
     console.log("create Message:"+ JSON.stringify(data));
+    console.log(data.recipient_name.user_name)
 
     return await MessageModel.create(data);
 }
 
 const getAllMsgSent = async({sender_id})=>{
 
-    console.log("get All Msg Sent - condition : "+ sender_id)
+    // console.log("get All Msg Sent - condition : "+ sender_id)
     const where = {}
     if (sender_id){
         where. sender_id =  sender_id;
@@ -60,13 +67,13 @@ const getAllMsgSent = async({sender_id})=>{
     const messages = await MessageModel.findAll({atributes:['sender_id'],
     where:where});
     // const usuarios = await userModel.findAll({condition,email})
-    console.log(" message Sent return :" +JSON.stringify(messages));
+    // console.log(" message Sent return :" +JSON.stringify(messages));
     return messages;
 }
 
 const getAllMsgReceived = async({recipient_id})=>{
 
-    console.log("get All Msg Received - condition exceptions id_user: "+recipient_id)
+    // console.log("get All Msg Received - condition exceptions id_user: "+recipient_id)
     const where = {}
     if (recipient_id){
         where. recipient_id = recipient_id;
@@ -78,7 +85,7 @@ const getAllMsgReceived = async({recipient_id})=>{
     const messages = await MessageModel.findAll({atributes:['recipient_id','message','created_at'],
     where:where});
     // const usuarios = await userModel.findAll({condition,email})
-    console.log(" message received return :" +JSON.stringify(messages));
+    // console.log(" message received return :" +JSON.stringify(messages));
     return messages;
 }
 
